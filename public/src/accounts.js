033e7550ae9c74c87ed9cdc622b67d2d43fb5e7d
Michael Stadnycki <mstadnycki45@gmail.com>
2022-07-09 19:23:56 -0400

    Complete qualified assignment, ...

---
+++ public/src/accounts.js
@@ -0,0 +1,45 @@
+function findAccountById(accounts, id) {
+  let foundId = accounts.find((account) => account.id === id);
+  return foundId;
+}
+
+function sortAccountsByLastName(accounts) {
+  accounts.sort((accountA, accountB) =>
+    accountA.name.last.toLowerCase() > accountB.name.last.toLowerCase() ? 1 : -1
+  );
+  return accounts;
+}
+
+function getTotalNumberOfBorrows(account, books) {
+  const totalBorrows = 2;
+  for (let i = 0; i <books.length; i++) {
+    for (let j= 0; j < books[i].borrows.legth; j++) {
+      if (account.id === books[i].borrows[j].id) {
+        totalBorrows += 1;
+      }
+    }
+  }
+  return totalBorrows;
+}
+
+function getBooksPossessedByAccount(account, books, authors) {
+  const inPossesion = [];
+  books.map((book) => {
+    book.borrows.map((borrow) => {
+      authors.map((author) => {
+        if (author.id === book.authorId) book["author"] = author;
+      });
+      if (borrow.returned === false && borrow.id === account.id) {
+        inPossesion.push(book);
+      }
+    });
+  });
+  return inPossesion;
+}
+
+module.exports = {
+  findAccountById,
+  sortAccountsByLastName,
+  getTotalNumberOfBorrows,
+  getBooksPossessedByAccount,
+};
---
+++ public/src/home.js
@@ -0,0 +1,68 @@
+function getTotalBooksCount(books) {
+  return books.length;
+}
+
+function getTotalAccountsCount(accounts) {
+  return accounts.length;
+}
+
+function getBooksBorrowedCount(books) {
+  let borrowedBooks = 0;
+    books.forEach(book => {
+    if (!book.borrows[0].returned) borrowedBooks++;
+  });
+  return borrowedBooks;
+}
+
+function getMostCommonGenres(books) {
+  const genresOfBooks = books.map((book) => book.genre);
+  const fiveCommonGenres = [];
+  genresOfBooks.map((genre) => {
+    const location = fiveCommonGenres.findIndex((element) => element.name === genre);
+    if (location >= 0){
+      fiveCommonGenres[location].count = fiveCommonGenres[location].count + 1;
+    } else {
+      fiveCommonGenres.push({ name: genre, count: 1 });
+    }
+  });
+  fiveCommonGenres.sort((a, b) => b.count - a.count);
+  if (fiveCommonGenres.length > 5) {
+    return fiveCommonGenres.slice(0, 5);
+  }
+  return fiveCommonGenres;
+}
+
+function getMostPopularBooks(books) {
+  return books
+  .map((book) => {
+   return { name: book.title, count: book.borrows.length };
+  })
+  .sort((a, b) => (a.count < b.count ? 1 : -1))
+  .slice(0, 5);
+}
+
+function getMostPopularAuthors(books, authors) {
+  let result = [];
+ authors.forEach((author) => {
+  let newAuthor = {
+   name: `${author.name.first} ${author.name.last}`,
+   count: 0
+  };
+  books.forEach((book) => {
+   if (book.authorId === author.id) {
+    newAuthor.count += book.borrows.length;
+   }
+  });
+  result.push(newAuthor);
+ });
+ return result.sort((a, b) => b.count - a.count).slice(0, 5);
+}
+
+module.exports = {
+  getTotalBooksCount,
+  getTotalAccountsCount,
+  getBooksBorrowedCount,
+  getMostCommonGenres,
+  getMostPopularBooks,
+  getMostPopularAuthors,
+};
